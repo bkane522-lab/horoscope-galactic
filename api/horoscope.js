44bb59e426875f7debeb3360bdce373f46a58733
bkane522-lab <bkane522@gmail.com>
2026-07-21 22:26:31 +0200

    Add files via upload

--- api/horoscope.js
+++ api/horoscope.js
@@ -1,9 +1,1 @@
-module.exports = async (req, res) => {
-  const sign = (req.query && req.query.sign) || 'Lion';
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.status(200).json({
-    sign,
-    message: `Cher ${sign}, aujourd’hui l’univers vous invite à ralentir pour mieux entendre votre véritable voix intérieure.`,
-    advice: 'Faites confiance à votre intuition, elle connaît le chemin.'
-  });
-};
+module.exports=(req,res)=>res.status(200).json({message:"Aujourd’hui, l’univers vous invite à ralentir pour entendre votre véritable voix intérieure.",advice:"Faites confiance à votre intuition."});
