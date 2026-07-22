cd76f82b35fc64624c1e7dc728174ffec54a5f90
bkane522-lab <bkane522@gmail.com>
2026-07-22 07:03:54 +0200

    Delete api/numerologie.js

--- api/numerologie.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handler(req, res) {
-  const date = (req.query.date || "").toString();
-  const digits = date.replace(/\D/g, "").split("").map(Number);
-  const total = digits.reduce((a, b) => a + b, 0);
-  const number = total ? ((total - 1) % 9) + 1 : 7;
-  res.status(200).json({ number, text: `Votre vibration du jour est ${number}.` });
-};
