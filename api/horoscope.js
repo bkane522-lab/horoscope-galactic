216c04be367cfeacbe69095886a8ae054368f23c
bkane522-lab <bkane522@gmail.com>
2026-07-22 07:03:40 +0200

    Delete api/horoscope.js

--- api/horoscope.js
+++
@@ -1,1 +0,0 @@
-module.exports=(req,res)=>res.status(200).json({message:"Aujourd’hui, l’univers vous invite à ralentir pour entendre votre véritable voix intérieure.",advice:"Faites confiance à votre intuition."});
