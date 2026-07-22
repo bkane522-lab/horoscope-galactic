53c1cf0e7f4ad971ca17c76be70c03d80aaf5729
bkane522-lab <bkane522@gmail.com>
2026-07-22 07:04:10 +0200

    Delete api/theme-astral.js

--- api/theme-astral.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res) => {
-  const sign = (req.query && req.query.sign) || 'Lion';
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.status(200).json({ sign, soleil: sign, lune: 'Intuition', ascendant: 'Royal', element: 'Or solaire' });
-};
