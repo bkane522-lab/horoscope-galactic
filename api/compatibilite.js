7092ffe5bd1c71b2e5aa60f0121d8941a1900d07
bkane522-lab <bkane522@gmail.com>
2026-07-22 07:03:31 +0200

    Delete api/compatibilite.js

--- api/compatibilite.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-  const a = (req.query && req.query.a) || 'Lion';
-  const b = (req.query && req.query.b) || 'Taureau';
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.status(200).json({ a, b, score: 82, resume: 'Connexion solide et bienveillante.' });
-};
